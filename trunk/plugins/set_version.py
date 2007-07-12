bc091f61278b3273d8347454e1bf0a321a4ea590
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-07-12 19:30:23 +0000

    deleting

--- trunk/plugins/set_version.py
+++
@@ -1,9 +0,0 @@
-#coding=utf8
-def fset_version(type,source,body):
- if new_access(source[1]+u"/"+source[2])>40:
-  write_file("dynamic/version.py",body[body.find(" ")+1:]);
-  iq.version=read_file("dynamic/version.py");
-  smsg(type,source,"saved");
- else:
-  smsg(type,source,"denied");
-newreg({"func":fset_version, "recmd": u"^set_version(\ .*)?$", "recmds": u"^set_version\ .+$","syn":"""set_version X.X.XX""","ctg":"admin", "name":"set_version","help":u""});
