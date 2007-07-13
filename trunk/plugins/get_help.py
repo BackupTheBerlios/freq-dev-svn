7d183ce904d5373e46e4de2a0464712af4f0aa9c
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-07-13 13:23:46 +0000

    0.1.179. None

--- trunk/plugins/get_help.py
+++ trunk/plugins/get_help.py
@@ -1,9 +1,10 @@
 import urllib;
 def get_help(p):
  try: h=CMDLIST[p]["help"];
- except: h='';
+ except: h=u'';
  try: h1=read_file("doc/man-ru/"+urllib.quote(str(p))+".txt");
- except: h1='';
+ except: h1=u'';
  if len(h1)>len(h): h=h1;
- if h: return (unicode(h), u"%s:\n%s" % (p, h))[int(p<>'404')];
+ h=unicode(h, "utf8");
+ if h: return (h, u"%s:\n%s" % (p, h))[int(p<>'404')];
  else: return get_help("404").replace("%p", p)
--- trunk/plugins/rewrite.py
+++ trunk/plugins/rewrite.py
@@ -3,7 +3,7 @@ REWRITELIST=load_info("dynamic/rewrite");
 def rewrite(body, gc):
  for i in get_rewrite_list(unicode(gc)):
   try:
-   p=re.search(u"^"+unicode(i[0])+u"$",unicode(body));
+   p=re.search(u"^"+unicode(i[0])+u"$",unicode(body), re.DOTALL);
   except:
    p=None;
   if p:
