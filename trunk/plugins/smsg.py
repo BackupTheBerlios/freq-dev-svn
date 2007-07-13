403416d6bc5ac233de3645c6bacaadcf32592968
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-07-13 16:30:50 +0000

    0.1.179. fixed typo in smsg (private msglimit)

--- trunk/plugins/smsg.py
+++ trunk/plugins/smsg.py
@@ -1,7 +1,7 @@
 #coding=utf8
 def smsg(type, source, body):
  if type=="private":
-  msg(source[0], body[:2000])
+  msg(source[0], body[:8000])
  else:
   if len(body)>get_msglimit(source[1]):
    msg(source[1],source[2]+u": см. приват");
