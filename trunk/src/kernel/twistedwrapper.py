5621cd5de8116363bf5c0c145d8082d9f8d0c3f8
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-08-06 11:01:24 +0000

    authenticated! :))

--- trunk/src/kernel/twistedwrapper.py
+++ trunk/src/kernel/twistedwrapper.py
@@ -1,14 +1,13 @@
 from twisted.words.protocols.jabber import client, jid, xmlstream
-#from twisted.protocols import xmlstream
-from twisted.xish import domish
+from twisted.words.xish import domish
 from twisted.internet import reactor
 import config
 
 class wrapper:
  def __init__(self):
-  self.jid = jid.Jid("%s@%s/%s" % (config.USER, config.SERVER, config.RESOURCE))
+  self.jid = jid.JID("%s@%s/%s" % (config.USER, config.SERVER, config.RESOURCE))
   self.c = client.basicClientFactory(self.jid, config.PASSWD)
-  self.c.addBootstrap(xmlstream.NS_AUTHD_EVENT, self.authd);
+  self.c.addBootstrap(xmlstream.STREAM_AUTHD_EVENT, self.authd);
   self.x=None
   reactor.connectTCP(config.SERVER, 5222, self.c) 
   reactor.run()
