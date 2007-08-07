2462a64d73805ead3a5f88988b66f2fce442efe5
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-08-07 11:03:16 +0000

    log & lang

---
+++ trunk/src/kernel/lang.py
@@ -0,0 +1,19 @@
+import os
+import config
+
+LANG={}
+ll=[i for i in os.listdir("lang") if i.endswith(".py")]
+for i in ll:
+ fp=file("lang/"+i, "r")
+ p=fp.read().split("\n")
+ p=[j.strip() for j in p if j.count(" ")]
+ fp.close()
+ for j in p:
+  k=j[:j.find(" ")]
+  v=j[j.find(" ")+1:]
+  if not LANG.has_key(i): LANG[i]={}
+  LANG[i][k]=v
+
+def get(m, l=config.LANG):
+ try: return LANG[l+".py"][m]
+ except: return "Lang.NotFound:%s:%s" % (l, m)
