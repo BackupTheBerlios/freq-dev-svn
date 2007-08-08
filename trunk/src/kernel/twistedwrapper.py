5b1bf451d3cd6ae38aaa4a42dd8a620a6f73db33
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-08-08 07:36:17 +0000

    handling all exceptions and writing them to ERRLOGFILE

--- trunk/src/kernel/twistedwrapper.py
+++ trunk/src/kernel/twistedwrapper.py
@@ -1,8 +1,11 @@
+import traceback
 from twisted.words.protocols.jabber import client, jid, xmlstream
 from twisted.words.xish import domish
 from twisted.internet import reactor
 import config
 import re
+import log
+import sys
 
 class wrapper:
  def __init__(self):
@@ -10,6 +13,7 @@ class wrapper:
   self.c = client.basicClientFactory(self.jid, config.PASSWD)
   self.c.addBootstrap(xmlstream.STREAM_AUTHD_EVENT, self.authd);
   self.x = None
+  self.log=log.logger()
   self.handlers = []
   self.msghandlers = []
   reactor.connectTCP(config.SERVER, 5222, self.c) 
@@ -23,7 +27,7 @@ class wrapper:
   p.addElement("status").addContent("some status")
   p.addElement("show").addContent("chat")
   self.x.send(p)
-  #self.x.send("<message to='freq2@burdakov.pp.ru'><body>msgtext</body></message>")   ## debug
+  self.x.send("<message to='freq2@burdakov.pp.ru'><body>msgtext</body></message>")   ## debug
   self.x.addObserver("/*", self.cb)
   self.x.addObserver("/message", self.cbmessage);
  def cb(self, x):
@@ -39,7 +43,7 @@ class wrapper:
   for i in self.handlers:
    if (i[0] in (n, None)) and (i[1] in (typ, None)) and (i[2] in (id, None)) and (i[3] in (body, None)) and (i[4] in (f, None)):
     if i[6]: self.handlers.remove(i)
-    reactor.callInThread(i[5], x);
+    reactor.callInThread(self.call, i[5], x);
  def register_handler(self, func, stanza=None, typ=None, id=None, body=None, f=None, once=None):
   self.handlers.append((stanza, typ, id, body, f, func, once))
  def register_msg_handler(self, func, body, typ=None, f=None):
@@ -53,7 +57,7 @@ class wrapper:
   except: typ="chat"
   for i in self.msghandlers:
    if re.search(i[1], body, re.DOTALL) and (i[2] in (typ, None)) and (i[3] in (f, None)):
-    reactor.callInThread(i[0], typ, f, body)
+    reactor.callInThread(self.call, i[0], typ, f, body)
  def send(self, x):
   reactor.callFromThread(self.x.send, x)
  def msg(self, typ, j, body, subject=None):
@@ -63,4 +67,11 @@ class wrapper:
   m.addElement("body").addContent(body)
   if subject: m.addElement("subject").addContent(subject)
   self.send(m)
+ def call(self, f, *args, **kwargs):
+  try: f(*args, **kwargs)
+  except:
+   m="".join(traceback.format_exception(sys.exc_type, sys.exc_value, sys.exc_traceback))
+   print "*STOP*: ", m
+   self.log.err(m)
+   reactor.stop()
 
