068f56226223c2834304dc3a5f401a321606cbeb
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-08-12 11:11:39 +0000

    msglimit (in MUC)

--- trunk/src/kernel/room.py
+++ trunk/src/kernel/room.py
@@ -1,3 +1,4 @@
+import options
 from item import item as titem
 
 class room:
@@ -13,3 +14,12 @@ class room:
   self.jid = jid
   self.setdefault = self.items.setdefault
   self.pop = self.items.pop
+ def get_option(self, k, d=None):
+  return options.get_option(self.jid, k, d)
+ def set_option(self, k, v):
+  options.set_option(self.jid, k, v)
+ def get_msglimit(self):
+  return int(self.get_option('msglimit', config.MSGLIMIT))
+ def set_msglimit(self, value):
+  return self.set_option('msglimit', str(value))
+
