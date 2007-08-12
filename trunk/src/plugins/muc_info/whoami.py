851eb0d31d206f3354e341c2f392678de1efe9dd
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-08-12 13:06:22 +0000

    new command whois; bugfix in whoami

--- trunk/src/plugins/muc_info/whoami.py
+++ trunk/src/plugins/muc_info/whoami.py
@@ -1,7 +1,8 @@
 def whoami_handler(t, item, params):
  if params: item.syntax(t, ".whoami")
- else: item.lmsg(t, "muc_whois", item.affiliation, item.role, item.show, item.status, time.strftime("%d.%m.%y %H:%M:%S"), item.access())
+ else: item.lmsg(t, "muc_whois", item.affiliation, item.role, item.show, item.status, time.strftime("%d.%m.%y %H:%M:%S", time.localtime(item.joined)), item.access())
 
 bot.register_cmd_handler(whoami_handler, ".whoami", g=1)
 
-# "g=1" means that this command avalable only in MUC
+# Note: "g=1" means that this command avalable only in MUC
+
