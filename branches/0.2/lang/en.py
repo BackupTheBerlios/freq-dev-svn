e89983b80e4826d8df74daf94bf2f31ba85b7546
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-08-14 13:23:04 +0000

    branches/0.2 deleted

--- branches/0.2/lang/en.py
+++
@@ -1,9 +0,0 @@
-log.record <p>[<i><font color="#999999">%d.%m.%y %H:%M:%S</font></i>] %%s</p>
-log.header <html><head><title>freq log</title></head><body>
-test.passed passed
-not_allowed Not allowed.
-invalid_syntax_default Invalid syntax
-pyexec.ok Successful!
-invalid_syntax %s
-muc_only this command available only in conference
-muc_whois %s/%s\n%s(%s)\njoined %s\naccess level: %d
