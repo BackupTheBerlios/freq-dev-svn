1baa4b249d71c5814fe29246734e0ae02af0a371
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-08-14 01:06:14 +0000

    bugfixes in en.py/ru.py

--- trunk/lang/en.py
+++ trunk/lang/en.py
@@ -5,6 +5,7 @@ invalid_syntax_default Invalid syntax
 lang_changed Language successfully changed
 lang_list Available languages: %s
 lang_msg_saved saved
+lang_not_found Language '%s' not found. Use .lang_list to see list of available languages.
 lang_self English (en)
 log.header <html><head><title>freq log</title></head><body>\n
 log.record <p>[<i><font color="#999999">%d.%m.%y %H:%M:%S</font></i>] %%s</p>\n
@@ -14,5 +15,4 @@ muc_whois %s/%s\n%s(%s)\njoined %s\naccess level: %s
 not_allowed Not allowed.
 pyexec.ok Successful!
 see_private see private
-lang_not_found Language '%s' not found. Use .lang_list to see list of available languages.
 test.passed passed
--- trunk/lang/ru.py
+++ trunk/lang/ru.py
@@ -2,17 +2,17 @@ current_lang текущий язык русский (ru)
 get_msglimit %s
 invalid_syntax %s
 invalid_syntax_default кривая команда
-lang_changed changed
+lang_changed язык изменен
 lang_list доступные языки - %s
-lang_msg_saved saved
+lang_msg_saved записано
+lang_not_found язык '%s' недоступен, или его не существует; используйте .lang_list для просмотра списка доступных языков.
 lang_self Русский (ru)
 log.header <html><head><title>freq log</title></head><body>\n
 log.record <p>[<i><font color="#999999">%d.%m.%y %H:%M:%S</font></i>] %%s</p>\n
-msglimit_saved saved
+msglimit_saved сохранено
 muc_only команда доступна только в конференции
 muc_whois %s/%s\n%s(%s)\nзашел в конфу %s\nуровень доступа: %s
 not_allowed фиг
 pyexec.ok выполнено
-see_private CMOTPi npiBAT
-lang_not_found %s: 404
+see_private глянь приват
 test.passed норма
