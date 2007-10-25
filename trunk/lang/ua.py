09631c88e7e7eca3b69c454daae60f8a07db9eb9
burdakov <burdakov@ea02f19d-1335-0410-a3c7-ac9331ffcd8c>
2007-10-25 15:29:44 +0000

    ~updated Ukrainian localization (thanks for 2pac)

--- trunk/lang/ua.py
+++ trunk/lang/ua.py
@@ -1,30 +1,30 @@
-alias_deleted удалил
-alias_empty нема жодного аліасу
-alias_not_found нема такого алиасу
+alias_deleted видалив
+alias_empty немає жодного аліасу
+alias_not_found такий аліас не існує
 alias_saved записав
-cleaned зачистка выполнена
+cleaned прибирання завершено
 current_lang поточна мова Українська (ua)
 disco_error хз
 from_me менe
 from_you тебе
 get_msglimit %s
 google? що шукати?
-google_history_notfound нема исторії пошука для 'next'
-google_no_results не знайдено
+google_history_notfound в історії пошуку немає 'next'
+google_no_results нічого не знайдено
 google_result %s\n%s (%s)
 google_results %s\n(всего %s результатов)
 groupchats зараз я сижу у %s\n(всего %s конференцій)
-help_categories Категорїї: %s\nщоб отримати перелік команд у категорїї, напишить 'help категорія', наприклад 'help basic'
-help_category Команди цієї категорїї: %s.\nДля допомоги пишить '.help команда', наприклад '.help test'
-help_not_found не знайдено (%s)
+help_categories Категорїї: %s\nщоб отримати перелік команд у категорїї, напишіть 'help категорія', наприклад 'help basic'
+help_category Команди в цій категорїї: %s.\nДля допомоги пишіть '.help команда', наприклад '.help test'
+help_not_found нічого не знайдено (%s)
 help_other_languages на жаль допомога по команді '%s' ще не написана на мові '%s', використовуйте %s
 help_saved ok
 help_show Категорїї: %s\n\n%s
 idle_error хз
 idle_result %s мовчить %s
-info я працюю без зупинок %s\nНавантаження: %s станзи у секунду, середня довжина станзи %s байт.
+info я працюю без зупинок %s\nНавантаження: %s станзи в секунду, середня довжина станзи %s байт.
 invalid_syntax синтаксис:\n%s
-invalid_syntax_default невірний синтаксис
+invalid_syntax_default некоректний синтаксис
 joined зайшов до '%s'
 lang_changed мову змінено
 lang_list доступні мови - %s
@@ -34,19 +34,19 @@ lang_self Українська (ua)
 leaved вийшов з '%s'
 log.header <html><head><title>freq log</title></head><body>\n
 log.record <p><i><font color="#999999">[%d.%m.%y %H:%M:%S]</font></i> %%s</p>\n
-moderate_error не получилося
-moderate_not_found кого?
+moderate_error не вдалося
+moderate_not_found кого??
 moderate_ok ok
-msglimit_saved сохранено
-muc_only команда доступна тільки у конференції
-muc_whois %s/%s\n%s(%s)\nзайшов до конфи %s\nрівень доступа: %s
+msglimit_saved збережено
+muc_only команда доступна тільки в конференції
+muc_whois %s/%s\n%s(%s)\nзайшов в конфу %s\nрівень доступу: %s
 nick_update ok
 nick_updated ok
-not_allowed фіґ
-ping_error не вийшло
+not_allowed фіґу
+ping_error не вдалося
 pong понг від %s %s
 pyexec.ok виконано
-see_private подивись приват
+see_private глянь в приват
 stats_error хз
 stats_result Статистика для %s\nЗагальна кількість: %s\nОнлайн: %s
 status_update ok
@@ -58,7 +58,7 @@ uptime_result %s працює без зупинок %s
 version_error не вийшло
 version_result_jid на %s %s %s %s
 version_result_nick %s використовує %s %s %s
-version_result_self я %s %s, запущен під %s
+version_result_self я %s %s, запущений під %s
 version_result_your в тебе %s %s %s
-whom? каво?
+whom? кого??
 ﻿alias_cleared очищено
